346c9cfeaf3f8838018708aa549236d81ede475d
Guilherme F Castro <gnmfcastro@gmail.com>
2018-09-02 22:53:48 -0300

    Adding more tests and fixing some corner cases

--- simpleChain.js
+++ simpleChain.js
@@ -24,30 +24,34 @@ class Blockchain {
   addBlock(newBlock) {
     return new Promise(resolve => {
       this.getBlockHeight()
-      .then(previousBlock => {
+      .then(height => {
+        newBlock.height = height + 1;
         newBlock.time = this.getCurrentTimeUTC();
-        newBlock.previousBlockHash = previousBlock ? previousBlock.hash : "";
-        newBlock.height = previousBlock ? previousBlock.height + 1 : 0;
-        newBlock.hash = this.generateBlockHash(newBlock);
-        chainDB.add(newBlock).then(() => {
-          resolve(newBlock.height);
-        })
-        .catch(error => {
-          console.error("Unable to add the block " + newBlock.height + "Due to error: ", error);
+        this.getBlock(height).then(previousBlock => {
+          newBlock.previousBlockHash = previousBlock ? previousBlock.hash : "";
+          newBlock.hash = this.generateBlockHash(newBlock);
+          chainDB.add(newBlock).then(() => {
+            resolve(newBlock.height);
+          })
+          .catch(error => {
+            console.error("Unable to add the block " + newBlock.height + "Due to error: ", error);
+          });
         });
       });
     });
   }
 
   getBlock(blockHeight) {
-    return chainDB.get(blockHeight);
+    return blockHeight >= 0 ? chainDB.get(blockHeight) : Promise.resolve();
   }
 
   getBlockHeight() {
     return new Promise((resolve, reject) => {
       chainDB.getCurrentBlock()
       .then(block => {
-        resolve(block.height);
+        let isFirstBlock = block ? false : true;
+        let blockHeight = isFirstBlock ? -1 : block.height;
+        resolve(blockHeight);
       })
       .catch(error => reject(error));
     });
@@ -118,10 +122,23 @@ class Blockchain {
 
 class Test{
 
-  constructor(){}
+  constructor(){
+    this.currentSize = 0;
+  }
 
   static setup() {
     this.bc = new Blockchain();
+  }
+
+  static before() {
+    return new Promise((resolve, reject) => {
+      this.bc.getBlockHeight()
+      .then(height => {
+        this.currentSize = height;
+        resolve(this.currentSize);
+      })
+      .catch(error => console.error("ERROR in geting block height"));
+    });
   }
 
   static happyPath() {
@@ -131,6 +148,14 @@ class Test{
           loop.bind(this)(i+1);
         } else {
           this.bc.validateChain();
+          this.bc.getBlockHeight()
+          .then(height => {
+            let expectedSize = this.currentSize + 10;
+            if(expectedSize !== height) {
+              console.error("ERROR");
+            }
+          })
+          .catch(error => console.error("ERROR in getting block height"));
         }
       });
     }).bind(this)(0);
@@ -138,7 +163,8 @@ class Test{
   
   static execute() {
     this.setup();
-    this.happyPath();
+    this.before()
+    .then(() => this.happyPath());
   }
 }
 
